7c1cdb76b22b6f09ace1eb54fc889b9ffabec834
Matvey <72608908+SMAY666@users.noreply.github.com>
2024-03-29 00:38:42 +0700

    fix(utils): fix generateUnicFilename method

--- src/utils/generateUnicFilename.ts
+++ src/utils/generateUnicFilename.ts
@@ -1,6 +1,6 @@
 import {v4 as uuidv4} from 'uuid';
-import path from 'node:path';
+import path from 'path';
 
 export function generateUnicFilename(originalname: string): string {
-    return uuidv4() + Date.now() + `.${path.extname(originalname)}`;
+    return uuidv4() + Date.now() + path.extname(originalname);
 }
