8cc7119223a1f922494f9147dac4e6b258aae5cb
Matvey <72608908+SMAY666@users.noreply.github.com>
2024-03-28 11:35:38 +0700

    refactor: generateUnicFilename function

--- src/utils/generateUnicFilename.ts
+++ src/utils/generateUnicFilename.ts
@@ -1,5 +1,6 @@
-import {uuid} from 'uuidv4';
+import {v4 as uuidv4} from 'uuid';
+import path from 'node:path';
 
-export function generateUnicFilename(): string {
-    return `${uuid()}${Date.now()}`;
+export function generateUnicFilename(originalname: string): string {
+    return uuidv4() + Date.now() + `.${path.extname(originalname)}`;
 }
