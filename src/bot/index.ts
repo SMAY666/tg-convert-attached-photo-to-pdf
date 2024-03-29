4ebe0319acb14329646d7a1489d8cdc8eb7424ec
Matvey <72608908+SMAY666@users.noreply.github.com>
2024-03-29 22:50:24 +0700

    feat(bot): add convert images to pdf logic

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -1,36 +1,39 @@
 import TelegramBot, {PhotoSize} from 'node-telegram-bot-api';
 import PDFDocument from 'pdfkit';
 import {ENV} from '../constants/env';
-import {createWriteStream, writeFileSync} from 'node:fs';
-import {sendRequest} from '../requests';
 
 import {generateUnicFilename} from '../utils/generateUnicFilename';
-import path from 'node:path';
-import {UPLOADS_DIR} from '../constants/path';
+import path from 'path';
+import {UPLOADS_DIR, USER_DIRECTORY} from '../constants/path';
+import * as fs from 'fs';
+import {createDir} from '../utils/fileTools';
 
 class Bot {
     constructor() {
         this.botInstance = new TelegramBot(ENV.BOT_TOKEN, {polling: true});
-        this.inProcess = false;
-        this.photos = null;
         this.commands = [
             {command: 'save', description: 'Выполните команду перед использованием бота'},
             {command: 'photos', description: 'Количество фотогпфий в списке'},
             {command: 'done', description: 'Выполните команду после отправки всех фотографий'},
+            {command: 'help', description: 'Список команд бота'},
         ];
+        this.inProcess = false;
+        this.photos = null;
+        this.serverPdfName = '';
     }
 
 
     // ----- [ PRIVATE MEMBERS ] ---------------------------------------------------------------------------------------
 
     private readonly botInstance: TelegramBot;
-    private inProcess: boolean;
-    private photos: (TelegramBot.PhotoSize & {filePath?: string})[] | null;
     private readonly commands: {
         command: string
         description: string
     }[];
 
+    private inProcess: boolean;
+    private photos: (TelegramBot.PhotoSize & {pathOnServer?: string})[] | null;
+    private serverPdfName: string;
 
     // ----- [ PRIVATE METHODS ] ---------------------------------------------------------------------------------------
 
@@ -43,22 +46,30 @@ class Bot {
         this.botInstance.on('message', async (message) => {
             if (!message.from?.username || !this.userHasAccess(message.from?.username)) {
                 await this.sendMessageToUser(message.chat.id, 'У вас нет доступа для взаимодействия со мной');
-            }
-            switch (message.text) {
-                case '/save':
-                    await this.activateSave('/save', message.chat.id);
-                    break;
-                case '/photos':
-                    await this.sendMessageToUser(message.chat.id, `Кол-во фотографий: ${this.photos?.length ?? '0'}`);
-                    break;
-                case '/done':
-                    await this.activateSave('/done', message.chat.id);
-                    break;
-                default:
-                    break;
-            }
-            if (this.inProcess && (message.text !== '/save' && message.text !== '/photos')) {
-                await this.getAttachments(message);
+            } else {
+                switch (message.text) {
+                    case '/start':
+                        await this.showInstruction(message.chat.id);
+                        await createDir(message.chat.id.toString());
+                        break;
+                    case '/help':
+                        await this.showInstruction(message.chat.id);
+                        break;
+                    case '/save':
+                        await this.activateSave(message.chat.id);
+                        break;
+                    case '/photos':
+                        await this.sendMessageToUser(message.chat.id, `Кол-во фотографий: ${this.photos?.length ?? '0'}`);
+                        break;
+                    case '/done':
+                        await this.doneSave(message.chat.id);
+                        break;
+                    default:
+                        break;
+                }
+                if (this.inProcess && (message.text !== '/save' && message.text !== '/photos' && message.text !== '/done')) {
+                    await this.getAttachments(message);
+                }
             }
         });
     }
@@ -73,37 +84,65 @@ class Bot {
         this.photos?.push(photoFromMessage);
     }
 
-    private async sendMessageToUser(chatId: number, test: string): Promise<void> {
-        await this.botInstance.sendMessage(chatId, test);
+    private async sendMessageToUser(chatId: number, text?: string, attachmentDocName?: string): Promise<void> {
+        if (text && !attachmentDocName) {
+            await this.botInstance.sendMessage(chatId, text);
+        }
+        if (attachmentDocName) {
+            try {
+                const stream = fs.createReadStream(path.join(UPLOADS_DIR, attachmentDocName));
+
+                await this.botInstance.sendDocument(chatId, stream, {
+                    ...(text ? {caption: text} : {}),
+                }, {
+                    filename: 'img2pdfConverter.pdf',
+                });
+            } catch (error) {
+                await this.sendMessageToUser(chatId, 'Извините. Что-то пошло не так..');
+            }
+        }
     }
 
-    private async activateSave(command: '/save' | '/done', chatId: number): Promise<void> {
-        if (command === '/save') {
-            if (this.inProcess) {
-                await this.sendMessageToUser(chatId, 'Можете отправлять фотографии');
-            }
-            this.photos = [];
-            this.inProcess = true;
-            await this.sendMessageToUser(chatId, 'Теперь отправьте мне фотографии, которые нужно сохранить');
-        } else if (command === '/done') {
-            if (!this.inProcess) {
-                await this.sendMessageToUser(chatId, 'Сохранение уже завершено');
-            }
-            if (this.photos && this.photos.length > 0) {
-                await this.downloadFile(this.photos[0]);
-            } else {
-                await this.sendMessageToUser(chatId, 'PDF файл не будет создан так как вы не добавили фотографии');
-            }
-            this.photos = null;
-            this.inProcess = false;
-            await this.sendMessageToUser(chatId, 'Сохранение завершено');
+    private async showInstruction(chatId: number): Promise<void> {
+        await this.sendMessageToUser(chatId, 'Вот список команд бота. Для удобства используйте кнопку "Menu"');
+        for (const command of this.commands) {
+            await this.sendMessageToUser(chatId, `${command.command} - ${command.description}`);
         }
+    }
+
+    private async activateSave(chatId: number): Promise<void> {
+        if (this.inProcess) {
+            await this.sendMessageToUser(chatId, 'Можете отправлять фотографии');
+        }
+        this.photos = [];
+        this.inProcess = true;
+        await this.sendMessageToUser(chatId, 'Теперь отправьте мне фотографии, которые нужно сохранить');
+    }
+
+
+    private async doneSave(chatId: number): Promise<void> {
+        if (!this.inProcess) {
+            await this.sendMessageToUser(chatId, 'Сохранение уже завершено');
+        }
+        await this.sendMessageToUser(chatId, 'Ожидайте ваш файл. Это займёт некоторое время');
+        if (this.photos && this.photos.length > 0) {
+            for (const photo of this.photos) {
+                await this.downloadFile(photo, UPLOADS_DIR);
+            }
+            await this.getPdf(this.photos, chatId);
+            await this.sendMessageToUser(chatId, ' Вот ваш файл. Спасибо за использование бота!\n' +
+                'Если во время работы что-то пошло не так, пожалуйства, напишите сюда @sm4yy', this.serverPdfName);
+        } else {
+            await this.sendMessageToUser(chatId, 'PDF файл не будет создан так как вы не добавили фотографии');
+        }
+        this.photos = null;
+        this.inProcess = false;
     }
 
 
     // ----- [ PUBLIC METHODS ] ----------------------------------------------------------------------------------------
 
-    public async start() {
+    public async start(): Promise<void> {
         await this.botInstance.setMyCommands(this.commands);
         console.log('Bot started');
         this.checkUpdates();
@@ -112,56 +151,53 @@ class Bot {
 
     // ----- [ HELP METHODS ] ------------------------------------------------------------------------------------------
 
-    private async getFilePath(photo: PhotoSize): Promise<string> {
-        if (!this.photos) {
-            throw new Error('Комманда \"/save\" не была выполнена');
-        }
-        const response = await sendRequest(
-            'GET',
-            `${ENV.API_URL}/bot${ENV.BOT_TOKEN}/getFile`,
-            {file_id: photo.file_id},
-        );
+    private async downloadFile(photo: PhotoSize & {pathOnServer?: string}, dirToSave: string): Promise<void> {
+        try {
+            const newFile = await this.botInstance.downloadFile(photo.file_id, dirToSave);
 
-        if (response.data.ok === false) {
-            throw new Error('Request failed:', response.data.result);
-        }
+            const filename = generateUnicFilename('photo.jpg').split('.')[0];
+            photo.pathOnServer = path.join(dirToSave, filename + path.extname(newFile));
 
-        const index = this.photos?.findIndex((tgData) => tgData === photo);
-
-        if (index === -1) {
-            throw Error('Фотография не из текущей сессии');
-        }
-
-        const foundPhoto = this.photos[index];
-
-        foundPhoto.filePath = response.data.result.file_path;
-
-        // @ts-ignore
-        return foundPhoto.filePath;
-    }
-
-    private async downloadFile(photo: PhotoSize) {
-        const filePath = await this.getFilePath(photo);
-
-        const response = await sendRequest(
-            'GET',
-            `${ENV.API_URL}/file/bot${ENV.BOT_TOKEN}/${filePath}`,
-            undefined,
-            undefined,
-            'arraybuffer',
-        );
-
-        if (response.status === 200) {
-            writeFileSync(path.join(
-                UPLOADS_DIR,
-                generateUnicFilename(filePath.replace('/', '-'))), response.data);
+            fs.rename(newFile, photo.pathOnServer, (error) => {
+                if (error) {
+                    throw new Error(`error: ${error}`);
+                }
+            });
+        } catch (error) {
+            throw new Error(`[downloadFiles]: Failed to download file ${error}`);
         }
     }
 
-    /* private async saveToPdf(photos: PhotoSize[]) {
-        const document = new PDFDocument();
-        document.pipe(createWriteStream('yourImages.pdf'));
-    } */
+    private getPdf(photos: (PhotoSize & {filePath?: string, pathOnServer?: string})[], chatId: number) {
+        const filename = generateUnicFilename('YourPdf.pdf');
+
+        return new Promise((resolve, reject) => {
+            try {
+                const document = new PDFDocument({autoFirstPage: false});
+                const stream = fs.createWriteStream(path.join(UPLOADS_DIR, filename));
+
+                stream.on('finish', () => {
+                    this.serverPdfName = filename;
+                    resolve(this.serverPdfName);
+                });
+
+                document.pipe(stream);
+
+                photos.forEach((photo) => {
+                    document.addPage();
+                    if (photo.pathOnServer) {
+                        document.image(photo.pathOnServer, 0, 0, {
+                            width: document.page.width,
+                            height: document.page.height,
+                        });
+                    }
+                });
+                document.end();
+            } catch (error) {
+                reject(new Error(`[saveToPdf]: Failed to save photos to pdf ${error}`));
+            }
+        });
+    }
 
 }
 
--- src/constants/path.ts
+++ src/constants/path.ts
@@ -1,5 +1,6 @@
-import path from 'node:path';
+import path from 'path';
 
 export const ROOT_DIR = process.cwd();
 export const PUBLIC_DIR = path.join(ROOT_DIR, 'public');
 export const UPLOADS_DIR = path.join(PUBLIC_DIR, 'uploads');
+export const USER_DIRECTORY = (chatId: number): string => path.join(UPLOADS_DIR, chatId.toString());
